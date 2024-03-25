f3192ff411f810df3c68f1ab1d3db312ca2aebf0
Zoha Khan <zohakhan11004@gmail.com>
2024-03-25 10:51:59 +0500

    files uploaded

---
+++ main.js
@@ -0,0 +1,30 @@
+//Example 1
+// let x=10
+// x +=5
+// console.log((x));
+//Example 2
+// let pizzaPrice:number=1000;
+// let tax:number=150;
+// let totalPrice=pizzaPrice+tax //longway
+// pizzaPrice+=tax;  //short way
+// console.log(totalPrice); //1000+150=1150
+//Example 3
+//==
+// let dataBasePassword:string="12345";
+// let maraPsswordJoMaDaalRahaHo:string ="123456";
+// console.log(dataBasePassword == maraPsswordJoMaDaalRahaHo); //false
+//===
+// let dataBasePassword:string="12345";
+// let maraPsswordJoMaDaalRahaHo:any =12345;
+// console.log(dataBasePassword == maraPsswordJoMaDaalRahaHo); //true equal value
+// console.log(dataBasePassword === maraPsswordJoMaDaalRahaHo); //false equal type
+// != ,!==
+// let Password:string="12345";
+// let confirmPassword:string ="12345";
+// console.log(Password != confirmPassword); //false equal value
+// console.log(Password !== confirmPassword);//false equal type
+// <
+var text1 = "A";
+var text2 = "B";
+var result = text1 < text2;
+console.log(result);
---
+++ main.ts
@@ -0,0 +1,39 @@
+
+//Example 1
+// let x=10
+// x +=5
+// console.log((x));
+
+
+//Example 2
+// let pizzaPrice:number=1000;
+// let tax:number=150;
+// let totalPrice=pizzaPrice+tax //longway
+// pizzaPrice+=tax;  //short way
+// console.log(totalPrice); //1000+150=1150
+
+//Example 3
+//==
+// let dataBasePassword:string="12345";
+// let maraPsswordJoMaDaalRahaHo:string ="123456";
+// console.log(dataBasePassword == maraPsswordJoMaDaalRahaHo); //false
+
+//===
+// let dataBasePassword:string="12345";
+// let maraPsswordJoMaDaalRahaHo:any =12345;
+// console.log(dataBasePassword == maraPsswordJoMaDaalRahaHo); //true equal value
+// console.log(dataBasePassword === maraPsswordJoMaDaalRahaHo); //false equal type
+
+// != ,!==
+// let Password:string="12345";
+// let confirmPassword:string ="12345";
+// console.log(Password != confirmPassword); //false equal value
+// console.log(Password !== confirmPassword);//false equal type
+
+
+
+// <
+// let text1="A"
+// let text2="B"
+// let result= text1 < text2
+// console.log(result);
